476cdcd72035c89bc0d3ca1a6a6cbaf59646261d
Aidan Killeen <aidan.killeen@gmail.com>
2021-11-03 16:00:38 +0000

    added request parameters

--- SpringWebApplication/src/main/java/ie/rc/SpringWebApplication/UserController.java
+++ SpringWebApplication/src/main/java/ie/rc/SpringWebApplication/UserController.java
@@ -5,8 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ie.rc.userdao.User;
 import ie.rc.userdao.UserDao;
@@ -45,6 +50,36 @@ public class UserController {
 		
 		return "userdetails";
 	}
+	
+	@RequestMapping("/details")
+	public String userDetailsUsingQueryParameter(Model model, @RequestParam int id) {
+		
+		try {
+			User u = dao.getUser(id);
+			model.addAttribute("user", u);
+			
+		} catch (UserDaoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "userdetails";
+	}
+
+	
+	@GetMapping("/add")
+	public String showAddUserForm() {
+		
+		return "userform";
+	}
+	
+	@PostMapping("/add")
+	public String handleAddUserFormSubmit(@ModelAttribute User userToAdd) {
+		
+		System.out.println(userToAdd);
+		
+		return "useradded";
+	}
+	
 	
 	
 	
